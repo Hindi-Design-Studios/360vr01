d32fb7885d6de9c4a6b448ac9c6fbc6cca93bc79
Hindi-Design-Studios <hindidesignstudio.net@gmail.com>
2024-10-09 23:09:54 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,109 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bedroom-360-vr-view",
+      "name": "Bedroom 360 Vr View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -1.5511013881540698,
+        "pitch": -0.028594193972217852,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04863104624704917,
+          "pitch": 0.10566465937643699,
+          "rotation": 0,
+          "target": "1-bathroom-360-vr-view"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05046699231146157,
+          "pitch": 0.2294787461037373,
+          "title": "Click Here&nbsp;",
+          "text": "Now Click on the up Arrow Button to See The Bathroom 360 vr View."
+        }
+      ]
+    },
+    {
+      "id": "1-bathroom-360-vr-view",
+      "name": "Bathroom 360 Vr View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.6717926317750713,
+        "pitch": 0.5775416141394132,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.020598299802077946,
+          "pitch": 0.02939907561737165,
+          "rotation": 0,
+          "target": "0-bedroom-360-vr-view"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.01754589953727148,
+          "pitch": 0.1805792703681277,
+          "title": "Click Here",
+          "text": "Now Click on the up Arrow Button to See The Bedroom 360 vr View."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
